afa4bc4a320eb9ac3588710d4e20a9db0abb7135
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2022-12-05 10:57:46 +0900

    Wrap decodeResponse and validateSAMLResponse in ExceptT

--- src/Network/Wai/SAML2/Validation.hs
+++ src/Network/Wai/SAML2/Validation.hs
@@ -53,13 +53,13 @@ validateResponse cfg responseData = runExceptT $ do
     -- get the current time
     now <- liftIO $ getCurrentTime
 
-    (responseXmlDoc, samlResponse) <- ExceptT $ decodeResponse responseData
-    ExceptT $ validateSAMLResponse cfg responseXmlDoc samlResponse now
+    (responseXmlDoc, samlResponse) <- decodeResponse responseData
+    validateSAMLResponse cfg responseXmlDoc samlResponse now
 
 -- | 'decodeResponse' @responseData@ decodes a SAML2 response contained
 -- in Base64-encoded @responseData@.
-decodeResponse :: BS.ByteString -> IO (Either SAML2Error (XML.Document, Response))
-decodeResponse responseData = runExceptT $ do
+decodeResponse :: BS.ByteString -> ExceptT SAML2Error IO (XML.Document, Response)
+decodeResponse responseData = do
     -- the response data is Base64-encoded; decode it
     let resXmlDocData = BS.decodeLenient responseData
 
@@ -83,8 +83,8 @@ validateSAMLResponse :: SAML2Config
                      -> XML.Document
                      -> Response
                      -> UTCTime
-                     -> IO (Either SAML2Error Assertion)
-validateSAMLResponse cfg responseXmlDoc samlResponse now = runExceptT $ do
+                     -> ExceptT SAML2Error IO Assertion
+validateSAMLResponse cfg responseXmlDoc samlResponse now = do
 
     -- check that the response indicates success
     case responseStatusCode samlResponse of
